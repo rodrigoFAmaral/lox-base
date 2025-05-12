6b7d9e154dd1d5b97c2b9e44a6514a4d3af8b83f
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2025-05-12 16:04:29 -0300

    Adicionou cursor para navegar nas árvores sintáticas

--- lox/node.py
+++ lox/node.py
@@ -1,7 +1,25 @@
-from abc import ABC
-from typing import Any, Callable, Iterator
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
 from functools import singledispatch
 from types import BuiltinFunctionType, FunctionType, MethodDescriptorType, MethodType
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    Generic,
+    Iterable,
+    Iterator,
+    Optional,
+    TypeVar,
+    Self,
+    cast,
+)
+
+if TYPE_CHECKING:
+    from .ast import Class, Function
+
+
+N = TypeVar("N", bound="Node", contravariant=True)
 
 
 class Node(ABC):
@@ -13,6 +31,7 @@ class Node(ABC):
     criar subclasses que implementem os métodos abstratos definidos aqui.
     """
 
+    @abstractmethod
     def eval(self, ctx):
         raise NotImplementedError("Método eval não implementado!")
 
@@ -41,7 +60,7 @@ class Node(ABC):
                 return False
         return True
 
-    def _pretty_lines(self, indent_level: int = 0) -> Iterator[tuple[int, str]]:
+    def _pretty_lines(self, indent_level: int = 0, end="") -> Iterator[tuple[int, str]]:
         """
         Método auxiliar para imprimir a árvore sintática de forma legível.
 
@@ -72,25 +91,56 @@ class Node(ABC):
             # Se o valor for um objeto do tipo `Node`, chamamos o método `pretty_lines`
             # recursivamente. Caso contrário, usamos a implementação genérica
             # do método `pretty`
-            if not isinstance(value, Node):
+            if isinstance(value, Node):
+                yield from self._pretty_lines_node(attr + "=", value, indent_level)
+            elif isinstance(value, (list, tuple)):
+                yield from self._pretty_lines_list(attr, value, indent_level)
+            else:
                 yield indent_level + 1, f"{attr}={pretty(value)}"
                 continue
 
-            lines = iter(value._pretty_lines(indent_level + 1))
-            (_, first_line) = next(lines)
-
-            # Tratamos a primeira linha de forma diferente, pois ela é prefixada
-            # com o nome do atributo.
-            yield indent_level + 1, f"{attr}={first_line}"
-
-            # O comando "yield from" gera todos os valores remanescentes do
-            # iterador `lines`.
-            yield from lines
-
         # Terminamos fechando o parênteses que foi aberto na primeira linha
-        yield indent_level, ")"
+        yield indent_level, ")" + end
 
-    def visit(self, visitors: dict[type, Callable[["Node"], Any]]) -> None:
+    def _pretty_lines_node(
+        self,
+        prefix: str,
+        value: "Node",
+        indent_level: int = 0,
+    ) -> Iterator[tuple[int, str]]:
+        """
+        Método auxiliar para imprimir nós de forma legível.
+        """
+        lines = iter(value._pretty_lines(indent_level + 1))
+        (_, first_line) = next(lines)
+
+        # Tratamos a primeira linha de forma diferente, pois ela é prefixada
+        # com o nome do atributo.
+        yield indent_level + 1, prefix + first_line
+
+        # O comando "yield from" gera todos os valores remanescentes do
+        # iterador `lines`.
+        yield from lines
+
+    def _pretty_lines_list(
+        self,
+        attr: str,
+        value: Iterable[Any],
+        indent_level: int = 0,
+    ) -> Iterator[tuple[int, str]]:
+        """
+        Método auxiliar para imprimir listas de forma legível.
+        """
+        yield indent_level + 1, f"{attr}=["
+
+        for item in value:
+            if isinstance(item, Node):
+                yield from item._pretty_lines(indent_level + 2, end=",")
+            else:
+                yield indent_level + 2, pretty(item) + ","
+        yield indent_level + 1, "]"
+
+    def visit(self, visitors: dict[type["Node"], Callable[[N], Any]]) -> None:
         """
         Recebe um dicionário de tipos associados a funções.
 
@@ -102,15 +152,221 @@ class Node(ABC):
             value = getattr(self, name)
             if isinstance(value, Node):
                 value.visit(visitors)
+            elif isinstance(value, (list, tuple)):
+                for item in value:
+                    if isinstance(item, Node):
+                        item.visit(visitors)
+                    else:
+                        visit_once(item, visitors)
+            else:
+                visit_once(value, visitors)
 
-        # Agora visitamos self, se necessário
-        for subtype in type(self).mro():
-            try:
-                visitor = visitors[subtype]
-                visitor(self)
-                break
-            except KeyError:
-                continue
+        # Agora visitamos self
+        visit_once(self, visitors)
+
+    def children(self) -> Iterable["Node"]:
+        """
+        Retorna todos os filhos do nó atual.
+
+        O método `children` retorna um iterador que percorre todos os filhos
+        do nó atual. Isso é útil para percorrer a árvore sintática de forma
+        recursiva.
+        """
+        for name in self.__annotations__:
+            value = getattr(self, name)
+            if isinstance(value, Node):
+                yield value
+            elif isinstance(value, (list, tuple)):
+                for item in value:
+                    if isinstance(item, Node):
+                        yield item
+
+    def descendants(self) -> Iterable[Any]:
+        """
+        Retorna todos os descendentes do nó atual.
+
+        O método `descendants` retorna um iterador que percorre todos os
+        descendentes do nó atual. Isso é útil para percorrer a árvore sintática
+        de forma recursiva.
+        """
+        yield self
+        for child in self.children():
+            yield from child.descendants()
+
+    def cursor(self) -> "Cursor[Self]":
+        """
+        Retorna um cursor para o nó atual.
+
+        O método `cursor` retorna um cursor para o nó atual. Isso é útil
+        para navegar na árvore sintática de forma recursiva.
+        """
+        return Cursor(self)
+
+    def replace_child(self, old: "Node", new: "Node") -> None:
+        """
+        Substitui um filho do nó atual.
+
+        O método `replace_child` substitui um filho do nó atual por um novo
+        nó. Isso é útil para modificar a árvore sintática de forma recursiva.
+        """
+        for name in self.__annotations__:
+            value = getattr(self, name)
+            if isinstance(value, Node):
+                if value is old:
+                    setattr(self, name, new)
+                    return
+            elif isinstance(value, (list, tuple)):
+                for i, item in enumerate(value):
+                    if item is old:
+                        if isinstance(value, tuple):
+                            msg = f"Em {type(self).__name__}.{name}: esperava uma lista de filhos, mas encontrei uma tupla"
+                            raise TypeError(msg)
+                        value[i] = new
+                        return
+
+
+@dataclass
+class Cursor(Generic[N]):
+    """
+    Classe que representa um cursor para navegar na árvore sintática.
+    """
+
+    node: N
+    parent_cursor: Optional["Cursor[Node]"] = field(default=None, repr=False)
+
+    def parent(self) -> "Cursor[Node]":
+        """
+        Retorna o nó pai do cursor.
+
+        O método `parent` retorna o nó pai do cursor. Isso é útil para
+        navegar na árvore sintática de forma recursiva.
+        """
+        if self.parent_cursor is None:
+            raise ValueError("O cursor não tem pai")
+        return self.parent_cursor
+
+    def root(self) -> "Cursor[Node]":
+        """
+        Retorna o nó raiz do cursor.
+
+        O método `root` retorna o nó raiz do cursor. Isso é útil para
+        navegar na árvore sintática de forma recursiva.
+        """
+        if not self.parent_cursor:
+            return cast("Cursor[Node]", self)
+        return self.parent_cursor.root()
+
+    def is_root(self) -> bool:
+        """
+        Verifica se o cursor é o nó raiz.
+
+        O método `is_root` verifica se o cursor é o nó raiz. Isso é útil
+        para verificar se o cursor está no topo da árvore sintática.
+        """
+        return self.parent_cursor is None
+        # ou seja, se o cursor não tem pai
+
+    def parents(self) -> Iterable["Cursor[Node]"]:
+        """
+        Retorna todos os pais do nó atual.
+
+        O método `parents` retorna um iterador que percorre todos os
+        pais do nó atual. Isso é útil para navegar na árvore sintática
+        de forma recursiva.
+        """
+        parent = self.parent_cursor
+        while parent:
+            yield parent
+            parent = parent.parent_cursor
+
+    def siblings(self) -> Iterable["Cursor[Node]"]:
+        """
+        Retorna os irmãos do nó atual.
+
+        O método `siblings` retorna um iterador que percorre todos os
+        irmãos do nó atual. Isso é útil para navegar na árvore sintática
+        de forma recursiva.
+        """
+        if not self.parent_cursor:
+            return
+        for sibling in self.parent_cursor.node.children():
+            if sibling is not self.node:
+                yield Cursor(sibling, self.parent_cursor)
+
+    def children(self) -> Iterable["Cursor[Node]"]:
+        """
+        Retorna os filhos do nó atual.
+
+        O método `children` retorna um iterador que percorre todos os
+        filhos do nó atual. Isso é útil para navegar na árvore sintática
+        de forma recursiva.
+        """
+        self = cast("Cursor[Node]", self)
+        for child in self.node.children():
+            yield Cursor(child, self)
+
+    def descendants(
+        self, skip: Callable[["Cursor"], bool] | None = None, skip_self: bool = False
+    ) -> Iterable["Cursor[Node]"]:
+        """
+        Retorna todos os descendentes do nó atual.
+
+        O método `descendants` retorna um iterador que percorre todos os
+        descendentes do nó atual. Isso é útil para navegar na árvore sintática
+        de forma recursiva.
+        """
+        if skip is None or not skip(self):
+            if not skip_self:
+
+                yield cast("Cursor[Node]", self)
+            for child in self.children():
+                yield from child.descendants(skip)
+
+    def is_scoped_to(self, scope: type[Node]) -> bool:
+        """
+        Verifica se o nó atual está definido dentro de um escopo específico.
+
+        O método `is_scoped` verifica se o nó atual está dentro de um
+        escopo específico. Isso é útil para verificar se o nó atual
+        está dentro de uma classe ou função.
+        """
+        for parent in self.parents():
+            if isinstance(parent.node, scope):
+                return True
+        return False
+
+    def class_scope(self) -> "Cursor[Class]":
+        """
+        Retorna um cursor para o nó de classe pai do nó atual.
+        """
+        from .ast import Class
+
+        for parent in self.parents():
+            if isinstance(parent.node, Class):
+                return parent
+        raise ValueError("O cursor não está dentro de uma classe")
+
+    def function_scope(self, root=False) -> "Cursor[Function]":
+        """
+        Retorna um cursor para o nó de função pai do nó atual.
+
+        Se `root` for True, retorna o nó de função raiz. Isso pode ser útil em
+        funções definidas dentro de outras funções. Por padrão, este método retorna
+        a função mais próxima do nó atual. Se root=True, retorna a função mais
+        pŕoxima da raiz.
+        """
+        from .ast import Function
+
+        cursor = None
+        for parent in self.parents():
+            if isinstance(parent.node, Function):
+                cursor = parent
+                if not root:
+                    return cursor
+
+        if cursor is None:
+            raise ValueError("O cursor não está dentro de uma função")
+        return cursor
 
 
 @singledispatch
@@ -149,3 +405,16 @@ def _(obj) -> str:
     Aqui chamamos o método `pretty` da classe base `Node`.
     """
     return obj.__name__
+
+
+def visit_once(obj: Node, visitors: dict[type[Node], Callable[[N], Any]]) -> None:
+    """
+    Visita um nó e executa a primeira função consistente com o tipo do objecto.
+    """
+    for subtype in type(obj).mro():
+        try:
+            visitor = visitors[subtype]
+            visitor(obj)  # type: ignore
+            break
+        except KeyError:
+            continue
